989bf89be2ed148828896dd85985249827ea14b2
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-17 15:29:05 +0630

    feat(): update account balance method added

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Querier interface {
+	AddAccountBalance(ctx context.Context, arg AddAccountBalanceParams) (Account, error)
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
 	GetAccount(ctx context.Context, id int64) (Account, error)
 	GetAccountForUpdate(ctx context.Context, id int64) (Account, error)
